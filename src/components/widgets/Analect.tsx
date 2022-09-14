d628743750a881eccd0fd3cfbf2d9ea6732b06cc
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2022-09-14 18:38:19 +0900

    クレジットSVGのバラス調整

--- src/components/widgets/Analect.tsx
+++ src/components/widgets/Analect.tsx
@@ -21,7 +21,7 @@ export const AnalectWidgetContainer = styled.View`
   position: absolute;
   left: 0;
   bottom: 0;
-  padding-right: 150px;
+  padding-right: 100px;
 `;
 
 export default AnalectWidget;
