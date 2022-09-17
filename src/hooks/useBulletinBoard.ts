fd7dd40fc0bd1ed8f1c957b025f3e077bd87ec6b
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2022-09-17 22:44:31 +0900

    伝言が変わったときの効果音

--- src/hooks/useBulletinBoard.ts
+++ src/hooks/useBulletinBoard.ts
@@ -1,12 +1,30 @@
 import { SUPABASE_KEY, SUPABASE_URL } from "@env";
 import { createClient } from "@supabase/supabase-js";
-import { useEffect, useState } from "react";
+import { Audio } from "expo-av";
+import { useCallback, useEffect, useState } from "react";
 import { BulletinBoardData } from "../models/BBPost";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 const useBulletinBoard = () => {
   const [posts, setPosts] = useState<BulletinBoardData[]>([]);
+  const [sound, setSound] = useState<Audio.Sound>();
+
+  const playBeep = useCallback(async () => {
+    const { sound } = await Audio.Sound.createAsync(
+      require("../assets/beep.mp3")
+    );
+    setSound(sound);
+    await sound.playAsync();
+  }, []);
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
 
   useEffect(() => {
     (async () => {
@@ -36,6 +54,7 @@ const useBulletinBoard = () => {
             default:
               break;
           }
+          playBeep();
         })
         .subscribe();
     })();
