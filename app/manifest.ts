9aecb1cf536f53b6cf6741b37ab95648f4aad776
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-11-05 19:20:43 +0900

    orientation: "landscape"

--- app/manifest.ts
+++ app/manifest.ts
@@ -9,6 +9,7 @@ export default function manifest(): MetadataRoute.Manifest {
     display: "standalone",
     background_color: "#ffffff",
     theme_color: "#171717",
+    orientation: "landscape",
     icons: [
       {
         src: "/icon-192x192.png",
