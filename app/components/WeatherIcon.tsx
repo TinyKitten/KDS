3b8e2e97831ab3e26ba4d0e9ab6e220a9fe36e79
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-11-06 01:58:12 +0900

    ダークモード対応

--- app/components/WeatherIcon.tsx
+++ app/components/WeatherIcon.tsx
@@ -11,25 +11,23 @@ type Props = {
 };
 
 export const WeatherIcon = ({ weatherCode }: Props) => {
-  const fill = "#171717";
-
   // https://open-meteo.com/en/docs
   switch (weatherCode) {
     case 0:
     case 1:
-      return <Sunny fill={fill} />;
+      return <Sunny />;
     case 2:
     case 3:
-      return <Cloudy fill={fill} />;
+      return <Cloudy />;
     case 45:
     case 48:
-      return <Foggy fill={fill} />;
+      return <Foggy />;
     case 51:
     case 53:
     case 55:
     case 56:
     case 57:
-      return <Drizzle fill={fill} />;
+      return <Drizzle />;
     case 61:
     case 63:
     case 65:
@@ -38,18 +36,18 @@ export const WeatherIcon = ({ weatherCode }: Props) => {
     case 80:
     case 81:
     case 82:
-      return <Rainy fill={fill} />;
+      return <Rainy />;
     case 71:
     case 73:
     case 75:
     case 77:
     case 85:
     case 86:
-      return <Snow fill={fill} />;
+      return <Snow />;
     case 95:
     case 96:
     case 99:
-      return <Thunderstorm fill={fill} />;
+      return <Thunderstorm />;
     default:
       return null;
   }
