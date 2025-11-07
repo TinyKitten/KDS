fba757055fef3f9172da0985918371292aa83e7f
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-11-07 20:39:37 +0900

    天気は１０分おきに更新

--- app/hooks/useWeather.ts
+++ app/hooks/useWeather.ts
@@ -1,15 +1,24 @@
 import { WeatherData } from "../domain/weather";
-import useSWR from "swr";
 import { weatherFetcher } from "../fetchers/weather";
+import useSWRImmutable from "swr/immutable";
+import { useEffect } from "react";
 
 export const useWeather = (
   latitude: number | null,
   longitude: number | null
 ) => {
-  const { data, error, isLoading } = useSWR<WeatherData>(
+  const { data, error, isLoading, mutate } = useSWRImmutable<WeatherData>(
     [latitude, longitude],
     weatherFetcher
   );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      mutate();
+    }, 10 * 60 * 1000); // 10分おきに更新
+
+    return () => clearInterval(interval);
+  }, [mutate]);
 
   return { data, error, isLoading };
 };
